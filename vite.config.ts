b1adc1dbc0aefe27b8e845b49916f123bf7577c4
Punit Dhoot <punit@Punits-Air.lan>
2024-08-26 19:29:04 +0530

    update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@emotion/react": "@emotion/react",
+      "@emotion/styled": "@emotion/styled",
+    },
+  },
   server: {
     proxy: {
       "/api": {
