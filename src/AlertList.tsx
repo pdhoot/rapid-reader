69e00eadd679c10c867d32b04f4821d6d2aa9c33
Punit Dhoot <punit@Punits-Air.lan>
2024-08-26 20:07:32 +0530

    remove subtext

--- src/AlertList.tsx
+++ src/AlertList.tsx
@@ -86,9 +86,9 @@ function AlertList({ key }: { key: string }) {
           <Paper sx={{ padding: "10px", width: "100%", maxWidth: "450px" }}>
             <ListItemText
               primary={feed.topic}
-              secondary={`Frequency: ${
-                feed.frequency
-              } | Last Updated: ${new Date(feed.lastUpdated).toLocaleString()}`}
+              //   secondary={`Frequency: ${
+              //     feed.frequency
+              //   } | Last Updated: ${new Date(feed.lastUpdated).toLocaleString()}`}
             />
             <ListItemSecondaryAction>
               <IconButton
