7da7109a3ba69595428b0efa0712a2dd6d089dc5
Punit Dhoot <punit@Punits-MacBook-Air.local>
2024-09-03 10:53:33 +0530

    force web browser instead of webview

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,33 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-);
+function Root() {
+  useEffect(() => {
+    const isWebView = () => {
+      const userAgent = navigator.userAgent.toLowerCase();
+      return /(webview|wv)/i.test(userAgent) || /linkedin/i.test(userAgent);
+    };
+
+    if (isWebView()) {
+      const openInBrowser = window.confirm(
+        "For the best experience, please open this app in a browser. Click OK to open in your default browser."
+      );
+      if (openInBrowser) {
+        window.open(window.location.href, "_system");
+      }
+    }
+  }, []);
+
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  );
+}
+
+createRoot(document.getElementById("root")!).render(<Root />);
