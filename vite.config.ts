367a06a7ab3f3229abfee2c456a4649ebe3b1267
Punit Dhoot <punitdhoot1@gmail.com>
2024-09-08 12:02:05 +0530

    update sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,46 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import sitemap from "vite-plugin-sitemap";
+import fs from "fs";
+import path from "path";
 
-// https://vitejs.dev/config/
+const hostname = "https://rapidread.io";
+
+// Function to generate sitemap index
+const generateSitemapIndex = () => {
+  const sitemapIndexContent = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>${hostname}/sitemap.xml</loc>
+  </sitemap>
+  <sitemap>
+    <loc>${hostname}/blog/sitemap.xml</loc>
+  </sitemap>
+</sitemapindex>`;
+
+  // Write sitemap index to file
+  fs.writeFileSync(
+    path.resolve(__dirname, "dist/sitemap-index.xml"),
+    sitemapIndexContent
+  );
+};
+
 export default defineConfig({
   plugins: [
     react(),
     sitemap({
-      hostname: "https://rapidread.io", // Replace with your domain
-      // additional options
+      hostname: hostname,
+      outDir: "./dist",
+      generateRobotsTxt: true,
+      exclude: ["/blog/**"], // Exclude blog routes from main sitemap
+      robots: [{ userAgent: "*", allow: "/" }],
     }),
+    {
+      name: "generate-sitemap-index",
+      closeBundle() {
+        generateSitemapIndex();
+      },
+    },
   ],
   resolve: {
     alias: {
@@ -24,6 +55,13 @@ export default defineConfig({
         changeOrigin: true,
         secure: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
       },
     },
   },
